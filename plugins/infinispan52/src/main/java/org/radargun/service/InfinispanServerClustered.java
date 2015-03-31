aefdc99051d149a833fedbb51da37cb8d1f118c1
Matej Cimbora <mcimbora@redhat.com>
2015-03-31 12:08:26 +0200

    Cluster formation fix

--- plugins/infinispan52/src/main/java/org/radargun/service/InfinispanServerClustered.java
+++ plugins/infinispan52/src/main/java/org/radargun/service/InfinispanServerClustered.java
@@ -52,20 +52,29 @@ public class InfinispanServerClustered implements Clustered {
       if (!service.lifecycle.isRunning()) {
          synchronized (this) {
             if (!membershipHistory.isEmpty() && membershipHistory.get(membershipHistory.size() - 1).members.size() > 0) {
+               lastMembers = null;
                membershipHistory.add(Membership.empty());
             }
          }
+         log.trace("Service is not running, returning empty members list.");
          return Collections.EMPTY_LIST;
       }
       try {
          MBeanServerConnection connection = service.connection;
-         if (connection == null) return null;
+         if (connection == null) {
+            log.trace("MBeanServerConnection not available, returning empty members list.");
+            return Collections.EMPTY_LIST;
+         }
          ObjectName cacheManagerName = new ObjectName(getCacheManagerObjectName(service.jmxDomain, service.cacheManagerName));
-         String membersString = (String) connection.getAttribute(cacheManagerName, getClusterMembersAttribute());
          String nodeAddress = (String) connection.getAttribute(cacheManagerName, getNodeAddressAttribute());
+         log.trace("Address returned by JMX query: " + nodeAddress);
+         String membersString = (String) connection.getAttribute(cacheManagerName, getClusterMembersAttribute());
+         log.trace("Members returned by JMX query: " + membersString);
          synchronized (this) {
             if (lastMembers != null && lastMembers.equals(membersString)) {
-               return membershipHistory.get(membershipHistory.size() - 1).members;
+               Collection membershipHistoryMembers = membershipHistory.get(membershipHistory.size() - 1).members;
+               log.trace(String.format("Last members %s not null, returning members %s from membership history.", lastMembers, membershipHistoryMembers));
+               return membershipHistoryMembers;
             }
             if (!membersString.startsWith("[") || !membersString.endsWith("]")) {
                throw new IllegalArgumentException("Unexpected members string format: " + membersString);
@@ -77,6 +86,7 @@ public class InfinispanServerClustered implements Clustered {
                members.add(new Member(nodes[i].trim(), nodes[i].equals(nodeAddress), i == 0));
             }
             membershipHistory.add(Membership.create(members));
+            log.trace("Returning members " + members);
             return members;
          }
       } catch (Exception e) {
